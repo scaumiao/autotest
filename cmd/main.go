71586ebeea142e2dc6706d46816eee31f20f0c94
scau_miao <scaumiao@gmail.com>
2019-02-20 11:32:33 +0800

    #6 add flag

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 
 	autotest "github.com/scaumiao/autotest"
 )
 
-func main() {
+var grpcPort = flag.String("grpc_port", "50051", "grpc port,default 50051")
+var gwPort = flag.String("gw_port", "8080", "grpc gateway port,default 8080")
 
-	autotest.NewServer()
+func main() {
+	flag.Parse()
+	autotest.NewServer(*grpcPort, *gwPort)
 
 }
 
