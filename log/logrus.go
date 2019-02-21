7e80086dca4dba6642389e31eb3e82095723a7aa
shampooed <1172767573@qq.com>
2019-02-21 10:07:16 +0800

    add log

---
+++ log/logrus.go
@@ -0,0 +1,26 @@
+package log
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// 设置日志格式为json格式
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// 设置将日志输出到标准输出（默认的输出为stderr，标准错误）
+	// 日志消息输出可以是任意的io.writer类型
+	log.SetOutput(os.Stdout)
+
+	// 设置日志级别为warn以上
+	log.SetLevel(log.WarnLevel)
+}
+
+func logger() {
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+}
